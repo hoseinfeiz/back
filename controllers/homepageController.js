dea8aa09d5613ad399e0316138ebdac8c1413fff
hosein feizresan <hosein.feiz@gmail.com>
2024-03-06 14:29:41 +0330

    after query checking on posts

--- controllers/homepageController.js
+++ controllers/homepageController.js
@@ -2,7 +2,9 @@ const Category = require('../models/Category')
 const Post = require('../models/Post')
 const homepageController = async (req, res) => {
     const cats = await Category.find({})
-    const posts = await Post.find({})
+    const posts = await Post.find({
+        category: { $in: ['US', 'Technology'] },
+    })
     res.render('homepage', {
         category: cats,
         posts,
